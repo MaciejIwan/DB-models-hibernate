6190f9990ec5a33f35d7c612e0031a64750f08c7
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-03-01 09:21:13 +0100

    init

---
+++ src/main/java/com/github/maciejiwan/investmens_tracking/InvestmensTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.github.maciejiwan.investmens_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmensTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmensTrackingApplication.class, args);
+    }
+
+}
