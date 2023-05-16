58087f179e9034545856ca78b1c585fee6a2707c
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-05-17 00:27:42 +0200

    rm task1 files

--- src/main/java/com/github/maciejiwan/investmens_tracking/repositories/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.maciejiwan.investmens_tracking.repositories;
-
-import com.github.maciejiwan.investmens_tracking.entities.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
--- src/main/java/com/github/maciejiwan/investmens_tracking/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.maciejiwan.investmens_tracking.repositories;
-
-import com.github.maciejiwan.investmens_tracking.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
